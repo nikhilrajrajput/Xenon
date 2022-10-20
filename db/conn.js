f86ce655dbf6fd15e4c6aea2b60aebfecc53cebf
nikhilrajrajput <96713971+nikhilrajrajput@users.noreply.github.com>
2022-10-20 07:04:23 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose =require("mongoose");
-mongoose.connect("mongodb://localhost:27017/xenonstack",{
+mongoose.connect("mongodb+srv://nikhilrajsingh787:Nikhil123@cluster0.i6th06s.mongodb.net/xenonstack",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     // useCreateIndex:true
