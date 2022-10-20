97482504c907f8b75b956d369eb2be29c0abdfb9
nikhilrajrajput <96713971+nikhilrajrajput@users.noreply.github.com>
2022-10-21 00:24:18 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose");
 mongoose.connect("mongodb+srv://nikhilrajsingh787:Nikhil123@cluster0.i6th06s.mongodb.net/xenonstack",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    // useCreateIndex:true
+    useCreateIndex:true
 }).then(()=>{
     console.log('Connnection Successful');
 }).catch((err)=>{
